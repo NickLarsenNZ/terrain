c7dc8683d353799729033f0d3b2453e360572bb8
Nick Larsen <nick.larsen@westpac.co.nz>
2019-07-31 15:51:30 +1200

    Test for utils.AppendError() fixes #13

--- utils/collections_test.go
+++ utils/collections_test.go
@@ -1,6 +1,7 @@
 package utils_test
 
 import (
+	"errors"
 	"github.com/nicklarsennz/terrain/utils"
 	"testing"
 )
@@ -16,5 +17,22 @@ func TestStringInSlice(t *testing.T) {
 
 	if utils.StringInSlice(expect_not_to_find, slice) {
 		t.Fatalf("expected not to find the string: '%s' in %v", expect_not_to_find, *slice)
+	}
+}
+
+func TestAppendError(t *testing.T) {
+	errs := &[]error{
+		errors.New("first error"),
+		errors.New("second error"),
+	}
+
+	expected_errors := len(*errs) + 1
+
+	utils.AppendError(errs, errors.New("third error"))
+
+	actual_errors := len(*errs)
+
+	if len(*errs) != expected_errors {
+		t.Fatalf("expected there to be %d errors, but only see %d", expected_errors, actual_errors)
 	}
 }
